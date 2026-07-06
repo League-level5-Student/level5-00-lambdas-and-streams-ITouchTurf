2ff6dae20b304391f8479c8928ff8593bcc3a65a
league <league@192.168.1.85>
2026-07-06 16:56:50 -0700

    start streams or take test up to u

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,14 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -17,6 +20,10 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
+		addNumbers.addActionListener(e -> System.out.println(addNumbers()));
+		randNumber.addActionListener(e -> System.out.println(randomNumber()));
+		tellAJoke.addActionListener(e -> System.out.println(joke()));
+		
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
@@ -25,8 +32,36 @@ public class LambdaButtons {
 		window.pack();
 	}
 	
+	private int randomNumber() {
+		Random r = new Random();
+		return r.nextInt(100);
+		// TODO Auto-generated method stub
+
+	}
+
+	private String joke() {
+		// TODO Auto-generated method stub
+		return "Why did the chicken cross the road?";
+	}
+
+	private String addNumbers() {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+		
+		int first = r.nextInt(100);
+		int second = r.nextInt(100);
+		int sum = first + second;
+		return (first + " + " + second + " = " + sum);
+	}
+
 	public static void main(String[] args) {
 		new LambdaButtons();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,29 +1,80 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
-		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
-				System.out.println(s);
+		//		printCustomMessage((s)->{
+		//			for(int i = 0; i < 10; i++) {
+		//				System.out.println(s);
+		//			}
+		//		}, "repeat");
+		//		
+		//		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
+		//		printCustomMessage((s)-> {
+		//			for(int i = 0; i>10; i--) {
+		//				for(int j = s.length(); j>0; j--) {
+		//				char[] grades = null;
+		//				grades[j]= s.charAt(j);
+		//				System.out.print(grades[j]);
+		//				}
+		//			}
+		//		}, "abcd");
+		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
+	
+
+		printCustomMessage((s)-> {
+	
+			String printed = "random uppercase";
+			char[] random = new char[printed.length()];
+			String result = "";
+			for(int i = 0; i<5; i++) {
+				 
+				for(int j = 0; j<printed.length(); j++) {
+
+					Random r = new Random();
+					if(r.nextBoolean()) {
+						random[j] = printed.toUpperCase().charAt(j);
+					} else {
+						random[j] = printed.charAt(j);
+					}
+
+				}
+				result = new String(random);
+				System.out.println(result);
 			}
 		}, "repeat");
-		
-		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
-		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)-> {
+			
+			String printed = "theres a period in between every character";
+			char[] random = new char[printed.length()*2];
+			String result = "";
+			for(int i = 0; i<5; i++) {
+				 
+				for(int j = 0; j<printed.length()*2; j++) {
+					if(j%2==0) {
+						random[j] = printed.charAt(j/2);
+					} else {
+						random[j] = '.';
+					}
+					
+				}
+				result = new String(random);
+				System.out.println(result);
+				}
+		}, "repeat");
+
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+
 	}
-	
+
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
 	}
 }
-	
+
